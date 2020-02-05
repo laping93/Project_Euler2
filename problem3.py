c1554e948d8a8d9f2c3d40590bf02b513527fc58
laping93 <egor.lapin2014@yandex.ru>
2020-02-05 23:30:46 +0700

    1

--- problem3.py
+++ problem3.py
@@ -1,1 +1,18 @@
-vbnvbnvbnvbnvbnvbn
+# The prime factors of 13195 are 5, 7, 13 and 29.
+# What is the largest prime factor of the number 600851475143 ?
+
+s = 0
+max_factor = 1
+i = 600851475142//2
+
+while True:
+    print(i)
+    if (i % 2 != 0) and (i % 3 != 0) and (i % 5 != 0) and (i % 7 != 0):
+        if 600851475143 % i == 0:
+            max_factor = i
+            break
+    i -= 1
+
+print('Максимальный делитель =', max_factor)
+
+# цикл равзернуть в другую сторону
